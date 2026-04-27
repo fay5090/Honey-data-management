34c66ffee66b1e0f87dde57ca6f059ef10d33cd9
fay5090 <faheemahmohamed6@gmail.com>
2026-04-27 19:16:41 +0300

    Refactor total calculation before fetch call

--- two.js
+++ two.js
@@ -1,3 +1,5 @@
+let total = Number(litres) * Number(price);
+
 fetch("https://script.google.com/macros/s/AKfycbyvkUqRK-gsNep0FKWmEQ9WauphmzxqPnFu8X4b2l_SkOscDtfIdcu2Xq8bAd8_Sw/exec", {
   method: "POST",
   body: JSON.stringify({
@@ -5,6 +7,6 @@ fetch("https://script.google.com/macros/s/AKfycbyvkUqRK-gsNep0FKWmEQ9WauphmzxqPn
     customer: name,
     litres: Number(litres),
     price: Number(price),
-    total: Number(litres) * Number(price)
+    total: total
   })
 });
