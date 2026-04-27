b1a8c395d31f93067dad90b36d6eeebbcb3c7bad
fay5090 <faheemahmohamed6@gmail.com>
2026-04-27 19:11:30 +0300

    Convert litres and price to numbers for calculation

--- two.js
+++ two.js
@@ -1,12 +1,10 @@
-let total = litres * price;
-
 fetch("https://script.google.com/macros/s/AKfycbyvkUqRK-gsNep0FKWmEQ9WauphmzxqPnFu8X4b2l_SkOscDtfIdcu2Xq8bAd8_Sw/exec", {
   method: "POST",
   body: JSON.stringify({
     batch: batch.bottles + "x" + batch.size + "L",
     customer: name,
-    litres: litres,
-    price: price,
-    total: total
+    litres: Number(litres),
+    price: Number(price),
+    total: Number(litres) * Number(price)
   })
 });
