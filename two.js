25833e94748dff636604c57f6856c628c777369a
fay5090 <faheemahmohamed6@gmail.com>
2026-04-27 19:23:41 +0300

    Refactor total calculation and string interpolation

--- two.js
+++ two.js
@@ -3,7 +3,7 @@ let total = Number(litres) * Number(price);
 fetch("https://script.google.com/macros/s/AKfycbyvkUqRK-gsNep0FKWmEQ9WauphmzxqPnFu8X4b2l_SkOscDtfIdcu2Xq8bAd8_Sw/exec", {
   method: "POST",
   body: JSON.stringify({
-    batch: batch.bottles + "x" + batch.size + "L",
+    batch: `${batch.bottles}x${batch.size}L`,
     customer: name,
     litres: Number(litres),
     price: Number(price),
