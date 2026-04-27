cf1686e52e95318e6c6abc470c910732e4783a32
fay5090 <faheemahmohamed6@gmail.com>
2026-04-27 18:49:05 +0300

    Remove headers from fetch request
    Removed headers from fetch request in two.js.

--- two.js
+++ two.js
@@ -1,10 +1,5 @@
-let total = litres * price;
-
 fetch("https://script.google.com/macros/s/AKfycbyvkUqRK-gsNep0FKWmEQ9WauphmzxqPnFu8X4b2l_SkOscDtfIdcu2Xq8bAd8_Sw/exec", {
   method: "POST",
-  headers: {
-    "Content-Type": "application/json"
-  },
   body: JSON.stringify({
     batch: batch.bottles + "x" + batch.size + "L",
     customer: name,
